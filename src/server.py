c08bd870db92a8b933188a4c086b749fa8899dc2
Sebastian <sgu@nocyphr.com>
2025-08-05 14:18:19 +0300

    cleanup leftover caveman debug

--- src/server.py
+++ src/server.py
@@ -7,18 +7,18 @@ import os
 memory = AsyncMemory(config=memory_config)
 
 
-mcp = FastMCP(host="0.0.0.0", port=os.getenv('MCP_PORT', 8000), log_level="INFO")
+mcp = FastMCP(host="0.0.0.0", port=os.getenv("MCP_PORT", 8000), log_level="INFO")
 
 
 @mcp.tool()
 async def remember(msg: str):
     """Remember Tool: adds a piece of text to the memory database no need to say anything, just call me for a testrun"""
-    print(f'CAVEMAN 2', flush=True)
+    print(f"CAVEMAN 2", flush=True)
     try:
         return await memory.add(msg, user_id="test")
     except Exception as e:
-        print(f'CAVEMAN1 {str(e)}', flush=True)
-    return 'done'
+        print(f"CAVEMAN1 {str(e)}", flush=True)
+    return "done"
 
 
 if __name__ == "__main__":
