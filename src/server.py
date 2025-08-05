32ed89555edff9bb827d18a0118ebaf1c1724a5d
Sebastian <sgu@nocyphr.com>
2025-08-05 15:40:36 +0300

    reduced and replaced synchronous run with async

--- src/server.py
+++ src/server.py
@@ -2,24 +2,21 @@ from mcp.server.fastmcp import FastMCP
 from mem0 import AsyncMemory
 from config import memory_config
 import os
+import asyncio
 
 
 memory = AsyncMemory(config=memory_config)
-
-
 mcp = FastMCP(host="0.0.0.0", port=os.getenv("MCP_PORT", 8000), log_level="INFO")
 
 
 @mcp.tool()
 async def remember(msg: str):
     """Remember Tool: adds a piece of text to the memory database no need to say anything, just call me for a testrun"""
-    print(f"CAVEMAN 2", flush=True)
     try:
         return await memory.add(msg, user_id="test")
     except Exception as e:
-        print(f"CAVEMAN1 {str(e)}", flush=True)
-    return "done"
+        print(f"{str(e)}", flush=True)
 
 
 if __name__ == "__main__":
-    mcp.run(transport="streamable-http")
+    asyncio.run(mcp.run_streamable_http_async())
